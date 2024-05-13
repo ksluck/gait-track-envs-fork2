6301b137d3358118b6970fff38e984bb064bba03
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-05-13 20:34:13 +0300

    fix(ant): set `_init_orientation` and `_init_height` in `__init__`

--- gait_track_envs/ant.py
+++ gait_track_envs/ant.py
@@ -1,6 +1,7 @@
+import gym
 import numpy as np
 from gym import utils
-import gym
+
 from .jinja_mujoco_env import MujocoEnv
 
 
@@ -12,9 +13,11 @@ class AntEnv(MujocoEnv, utils.EzPickle):
             self.current_lengths = np.array(self.original_lengths)
             self.model_args = {"size": list(self.original_lengths)}
         elif set_values == "all":
-            self.original_lengths = self.original_lengths.reshape(1, -1).repeat(4, axis=0)
+            self.original_lengths = self.original_lengths.reshape(1, -1).repeat(
+                4, axis=0
+            )
             self.current_lengths = np.array(self.original_lengths).flatten()
-            self.model_args = {"size": list(self.original_lengths)*4}
+            self.model_args = {"size": list(self.original_lengths) * 4}
         elif set_values == "none":
             self.current_lengths = np.array(self.original_lengths)
             self.model_args = {}
@@ -27,16 +30,21 @@ class AntEnv(MujocoEnv, utils.EzPickle):
         self.origin = "torso"
         self.set_values = set_values
 
-        MujocoEnv.__init__(self, 'ant.xml', 5)
+        self._init_orientation = np.array([0.0, 0.0, 0.0])
+        self._init_height = 0.0
+
+        MujocoEnv.__init__(self, "ant.xml", 5)
         utils.EzPickle.__init__(self)
 
-        self.min_task = self.original_lengths*0.5
-        self.max_task = self.original_lengths*2
+        self.min_task = self.original_lengths * 0.5
+        self.max_task = self.original_lengths * 2
 
     def get_test_tasks(self):
-        return {"normal": np.copy(self.original_lengths),
-                "short": self.original_lengths*0.5,
-                "long": self.original_lengths*2}
+        return {
+            "normal": np.copy(self.original_lengths),
+            "short": self.original_lengths * 0.5,
+            "long": self.original_lengths * 2,
+        }
 
     def set_random_task(self):
         self.set_task(*self.sample_task())
@@ -49,16 +57,30 @@ class AntEnv(MujocoEnv, utils.EzPickle):
         if self.set_values == "all":
             return self.current_lengths.reshape(4, -1) * np.sqrt(2)
         elif self.set_values == "same_only_back":
-            return np.stack([self.original_lengths, self.original_lengths, self.current_lengths, self.current_lengths]) * np.sqrt(2)
+            return np.stack(
+                [
+                    self.original_lengths,
+                    self.original_lengths,
+                    self.current_lengths,
+                    self.current_lengths,
+                ]
+            ) * np.sqrt(2)
         elif self.set_values == "same_only_front":
-            return np.stack([self.current_lengths, self.current_lengths, self.original_lengths, self.original_lengths]) * np.sqrt(2)
+            return np.stack(
+                [
+                    self.current_lengths,
+                    self.current_lengths,
+                    self.original_lengths,
+                    self.original_lengths,
+                ]
+            ) * np.sqrt(2)
         else:
             return self.current_lengths.reshape(1, -1).repeat(4, axis=0) * np.sqrt(2)
 
     @property
     def morpho_params(self):
         # TODO fix other cases
-        assert self.limb_segment_lengths.flatten().shape == (3, )
+        assert self.limb_segment_lengths.flatten().shape == (3,)
         return self.limb_segment_lengths.flatten()
 
     def sample_tasks(self, num_tasks=1):
@@ -71,25 +93,26 @@ class AntEnv(MujocoEnv, utils.EzPickle):
         self.current_lengths = np.copy(task)
         self.model_args["size"] = list(self.current_lengths)
         self.build_model()
-    
+
     def reset(self):
-    	ob, _ = MujocoEnv.reset(self)
-    	self._init_orientation = np.array(ob[1:4])
-    	self._init_height = ob[0]
-    	ob[0] = ob[0] / self._init_height
-    	return ob, {}
+        ob, _ = MujocoEnv.reset(self)
+        self._init_orientation = np.array(ob[1:4])
+        self._init_height = ob[0]
+        ob[0] = ob[0] / self._init_height
+        return ob, {}
 
     def step(self, a):
         xposbefore = self.get_body_com("torso")[0]
         self.do_simulation(a, self.frame_skip)
         xposafter = self.get_body_com("torso")[0]
         ob = self._get_obs()
-        
+
         orientation_reward = np.sum(np.array(ob[1:4]) * self._init_orientation) ** 2
-        forward_reward = (xposafter - xposbefore)/self.dt
-        ctrl_cost = .5 * np.square(a).sum()
-        contact_cost = 0.5 * 1e-3 * np.sum(
-            np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        forward_reward = (xposafter - xposbefore) / self.dt
+        ctrl_cost = 0.5 * np.square(a).sum()
+        contact_cost = (
+            0.5 * 1e-3 * np.sum(np.square(np.clip(self.sim.data.cfrc_ext, -1, 1)))
+        )
         survive_reward = 1.0
         reward = forward_reward - ctrl_cost - contact_cost + 0.5 * orientation_reward
         terminated = False
@@ -99,26 +122,37 @@ class AntEnv(MujocoEnv, utils.EzPickle):
         track_info = self.get_track_dict()
         ob[0] = ob[0] / self._init_height
 
-        return ob, reward, terminated, truncated, dict(
-            reward_run=forward_reward,
-            reward_ctrl=-ctrl_cost,
-            reward_contact=-contact_cost,
-            reward_survive=survive_reward,
-            terminated=terminated,
-            truncated=truncated,
-            **track_info)
+        return (
+            ob,
+            reward,
+            terminated,
+            truncated,
+            dict(
+                reward_run=forward_reward,
+                reward_ctrl=-ctrl_cost,
+                reward_contact=-contact_cost,
+                reward_survive=survive_reward,
+                terminated=terminated,
+                truncated=truncated,
+                **track_info,
+            ),
+        )
 
     def _get_obs(self):
-        return np.concatenate([
-            self.sim.data.qpos.flat[2:],
-            self.sim.data.qvel.flat,
-            #np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
-        ])
+        return np.concatenate(
+            [
+                self.sim.data.qpos.flat[2:],
+                self.sim.data.qvel.flat,
+                # np.clip(self.sim.data.cfrc_ext, -1, 1).flat,
+            ]
+        )
 
     def reset_model(self):
-        qpos = self.init_qpos + self.np_random.uniform(size=self.model.nq, low=-.1, high=.1)
+        qpos = self.init_qpos + self.np_random.uniform(
+            size=self.model.nq, low=-0.1, high=0.1
+        )
         qpos[1] += 2
-        qvel = self.init_qvel + self.np_random.standard_normal(self.model.nv) * .1
+        qvel = self.init_qvel + self.np_random.standard_normal(self.model.nv) * 0.1
         self.set_state(qpos, qvel)
         return self._get_obs()
 
@@ -141,8 +175,8 @@ def register_ant(env_name):
         raise ValueError("Unknown env name")
 
     gym.envs.register(
-            id=env_name,
-            entry_point="%s:AntEnv" % __name__,
-            max_episode_steps=1000,
-            kwargs=kwargs,
+        id=env_name,
+        entry_point="%s:AntEnv" % __name__,
+        max_episode_steps=1000,
+        kwargs=kwargs,
     )
